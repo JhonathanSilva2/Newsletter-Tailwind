491846a05e1b179811fadf800b67949f13592e9f
JhonathanSilva2 <jhonathancontato4@gmail.com>
2023-12-04 18:41:34 -0300

    Basic Tailwind course - First part
    In this commit, I've started the project by focusing on mobile-first design principles Alura course. Key considerations include:
    
    - Understanding mobile-first design principles for a better user experience on smaller screens.
    - Implementing responsive design with media queries for different screen sizes.
    - Gaining a foundational understanding of basic Taiwing principles.
    - Exploring fundamental Taiwing techniques and methodologies.
    
    This commit lays the foundation for the mobile layout, and further enhancements will be made in subsequent commits. Documentation will be updated accordingly."

---
+++ my-app/tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors:{
+        azul:{
+          claro:'#C5DFFF',
+          escuro:'#061E3C',
+          hover:'#0A2A5C',
+      }},
+      fontFamily:{
+        Dhurjati: ['Dhurjati', 'sans-serif']
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
